a6a93f637345c5b2110bd08003f2a2511af59c91
beks <foxanraven.a2@gmail.com>
2025-06-04 03:28:01 +0600

    '' redirect to admin

--- gitapp/urls.py
+++ gitapp/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('admin/')),  # редирект с / на /admin/
     path('admin/', admin.site.urls),
-    path('api/', include('learning.urls')),  # API маршруты из твоего приложения
+    path('api/', include('learning.urls')),  # ✅ Вставляем путь к приложению с router
 ]
 
-# Только в режиме разработки: обслуживать статику напрямую
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
