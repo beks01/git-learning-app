950b729ce65aac478f0fb9c3fae028b2fde449f0
beks <foxanraven.a2@gmail.com>
2025-06-04 08:31:01 +0600

    debug = true

--- learning/views.py
+++ learning/views.py
@@ -6,10 +6,28 @@ class GitCommandViewSet(viewsets.ModelViewSet):
     queryset = GitCommand.objects.all()
     serializer_class = GitCommandSerializer
 
+    def get_queryset(self):
+        language = self.request.query_params.get('language')
+        if language:
+            return self.queryset.filter(language=language)
+        return self.queryset
+
 class GitLessonViewSet(viewsets.ModelViewSet):
     queryset = GitLesson.objects.all()
     serializer_class = GitLessonSerializer
 
+    def get_queryset(self):
+        language = self.request.query_params.get('language')
+        if language:
+            return self.queryset.filter(language=language)
+        return self.queryset
+
 class GitQuizViewSet(viewsets.ModelViewSet):
     queryset = GitQuiz.objects.all()
     serializer_class = GitQuizSerializer
+
+    def get_queryset(self):
+        language = self.request.query_params.get('language')
+        if language:
+            return self.queryset.filter(language=language)
+        return self.queryset
